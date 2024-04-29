785bbd2c74dd0fda0b73f9989e7a3859a15134bd
irem acar <iremmacar35@gmail.com>
2024-04-29 21:22:59 +0300

    seccond commit

---
+++ DOMManipiasyonu/app.js
@@ -0,0 +1,25 @@
+//Session Storage - Key and Value
+
+//console.log(window);
+
+const add = document.querySelector("#add");
+const del = document.querySelector("#delete");
+const clear = document.querySelector("clear");
+
+const addkey = document.querySelector("#addkey");
+const addvalue = document.querySelector("#addvalue");
+const deletekey = document.querySelector("#deletekey");
+
+add.addEventListener("click",addItem);
+del.addEventListener("click",deleteItem);
+clear.addEventListener("click", clearItem);
+
+function addItem(e) {
+    sessionStorage.setItem(addkey.value,addvalue.value);
+}
+function deleteItem(e) {
+    
+}
+function clearItem(e) {
+    
+}
